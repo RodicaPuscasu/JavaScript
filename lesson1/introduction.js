d65988fd3b5c6062fa9e664701ea97eb5dfb08bd
RodicaPuscasu <150557402+RodicaPuscasu@users.noreply.github.com>
2024-03-01 07:15:26 +0200

    lesson 1 part 1

---
+++ lesson1/introduction.js
@@ -0,0 +1,30 @@
+console.log("Alright! Alright! Alright!");
+
+const age = 25; // Number
+const name = "Rodica"; // String
+const surname = 'Puscasu';
+const fullName = `${name} ${surname}`;
+console.log(fullName);
+const isFemale = true; // boolean
+
+const rodica = {
+    name: "Rodica",
+    surname: "Puscasu"
+
+}
+console.log(rodica.name);
+
+const ageOfUniverseInBillions = 1.8; //not sure
+
+let myAge;
+console.log(myAge);
+
+// alert ("Are you sure?")
+
+// const areYouSure = confirm("Are you sure?");
+// console.log(areYouSure)
+
+// const review = prompt("Please leave a review");
+// console.log(review)
+
+
---
+++ lesson1/strings.js
@@ -0,0 +1,5 @@
+const name = "Rodica"; 
+const surname = "Puscasu";
+const fullName = name + " " + surname;
+
+console.log("Length", fullName.length);
